9c442703935021670c0ea047348d452022a71092
Arkajyoti Saha <124441562+saha2023@users.noreply.github.com>
2023-02-14 15:18:55 +0000

    minHeightBST

---
+++ Mediam/minHeightBST.cpp
@@ -0,0 +1,45 @@
+using namespace std;
+
+class BST {
+public:
+  int value;
+  BST *left;
+  BST *right;
+
+  BST(int value) {
+    this->value = value;
+    left = nullptr;
+    right = nullptr;
+  }
+
+  void insert(int value) {
+    if (value < this->value) {
+      if (left == nullptr) {
+        left = new BST(value);
+      } else {
+        left->insert(value);
+      }
+    } else {
+      if (right == nullptr) {
+        right = new BST(value);
+      } else {
+        right->insert(value);
+      }
+    }
+  }
+};
+void recursive(int l, int r, BST* &root, vector<int>&arr){
+  if(l>=r) return;
+  int m = (l+r)/2;
+  if(!root) root = new BST(arr[m]);
+  else root->insert(arr[m]);
+  recursive(l, m, root, arr);
+  recursive(m+1, r, root, arr);
+}
+BST *minHeightBst(vector<int> arr) {
+  // Write your code here.
+  BST* root = nullptr;
+  int n = arr.size();
+  recursive(0, n, root, arr);
+  return root;
+}
