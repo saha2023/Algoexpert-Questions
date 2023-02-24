c37ce46731835ccc52e141d53022d790af89e403
arko <arkajyotisaha891@gmail.com>
2023-02-25 02:11:22 +0530

    lcs

--- Hard/longestCommonSubsequence.cpp
+++ Hard/longestCommonSubsequence.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 vector<char> longestCommonSubsequence(string str1, string str2)  {
     // Write your code here.
-    vector<vector<vector<char>>> lcs(str2.size()+1, vector<vector<char>>(str1.size()+1, vector<char>()));
+    vector<vector<string>> lcs(str2.size()+1, vector<string>(str1.size()+1));
     for (int i = 1; i <= str2.length(); i++) {
         for (int j = 1; j <= str1.length(); j++) {
             if (str2[i - 1] == str1[j - 1]) {
-                vector<char> copy = lcs[i - 1][j - 1];
+                string copy = lcs[i - 1][j - 1];
                 copy.push_back(str2[i - 1]);
                 lcs[i][j] = copy;
             }
@@ -16,5 +16,9 @@ vector<char> longestCommonSubsequence(string str1, string str2)  {
             }
         }
     }
-    return lcs[str2.length()][str1.length()];
+    vector<char>ans;
+    for(char c: lcs[str2.length()][str1.length()]){
+        ans.push_back(c);
+    }
+    return ans;
 }
